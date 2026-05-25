54b5617c006de0e6028c1e808ae6637a8987d8dc
Iome1790 <chawjohnchakma2007@gmail.com>
2026-05-25 14:06:16 +0530

    Add files via upload

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import App from "./App";
 import "./index.css";
 
@@ -10,4 +11,10 @@ document.addEventListener("DOMContentLoaded", () => {
   document.body.style.webkitTouchCallout = "none";
 });
 
-createRoot(document.getElementById("root")!).render(<App />);
+const manifestUrl = `${window.location.origin}/tonconnect-manifest.json`;
+
+createRoot(document.getElementById("root")!).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
+    <App />
+  </TonConnectUIProvider>
+);
