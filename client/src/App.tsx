c1a65f8840f56bb1d8e204c34dcf01ea4261381d
Iome1790 <chawjohnchakma2007@gmail.com>
2026-05-25 16:12:42 +0530

    Add files via upload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -22,10 +22,12 @@ declare global {
   }
 }
 
+const Home = lazy(() => import("@/pages/Home"));
 const Landing = lazy(() => import("@/pages/Landing"));
 const Admin = lazy(() => import("@/pages/Admin"));
 const CountryControls = lazy(() => import("@/pages/CountryControls"));
 const GamesPage = lazy(() => import("@/pages/Games"));
+const WithdrawPage = lazy(() => import("@/pages/Withdraw"));
 const EarnPage = lazy(() => import("@/pages/Earn"));
 const WatchPage = lazy(() => import("@/pages/Earn"));
 const FriendPage = lazy(() => import("@/pages/Friend"));
